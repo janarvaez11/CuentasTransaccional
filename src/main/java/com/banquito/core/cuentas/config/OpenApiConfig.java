6389863199665da2b488350156c22ad01a908cf7
GiovannaSoria <gmsoria@espe.edu.ec>
2025-08-10 19:20:27 -0500

    [UPD] Cluster and Swagger Config

--- src/main/java/com/banquito/core/cuentas/config/OpenApiConfig.java
+++ src/main/java/com/banquito/core/cuentas/config/OpenApiConfig.java
@@ -2,8 +2,11 @@ package com.banquito.core.cuentas.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -13,6 +16,14 @@ public class OpenApiConfig {
                 .info(new Info()
                         .title("API de Cuentas")
                         .version("v1")
-                        .description("Documentación de los endpoints de cuentas"));
+                        .description("Documentación de los endpoints de cuentas"))
+                .servers(List.of(
+                        new Server()
+                                .url("http://banquito-alb-1166574131.us-east-2.elb.amazonaws.com/api/cuentas")
+                                .description("Servidor AWS (Producción)"),
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("Servidor Local (Desarrollo)")
+                ));
     }
 }
